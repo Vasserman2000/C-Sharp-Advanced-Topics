4678925f8e93e970ac158617f1e8200fb19726ff
vasserman2000@gmail.com <vasserman2000@gmail.com>
2018-12-31 18:10:31 +0200

    Delegates and some concurrent programming added

---
+++ Delegates/Delegates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    /// <summary>
+    /// Delegate is an object that knows how to call a method (or a group of methods)
+    /// Delegate is a reference (or a pointer) to a function
+    /// Used for designing extensible and flexible applications
+    /// </summary>
+    public static class Delegates
+    {
+        
+    }
+}
