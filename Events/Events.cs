61ce6d960a8d19793e4284684beac4406c69e75b
vasserman2000@gmail.com <vasserman2000@gmail.com>
2019-01-08 18:02:47 +0200

    Events (initial commit)

---
+++ Events/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    /// <summary>
+    /// EVENTS:
+    /// - A mechanism for communication between objects
+    /// - Used in building Loosely Coupled Applications
+    /// - Helps extending application
+    /// </summary>
+    public class Events
+    {
+    }
+}
