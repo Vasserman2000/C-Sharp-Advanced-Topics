ce5eddf6aac0ac04cb8adb6a642cc6469c544509
Vasserman2000 <vasserman2000@gmail.com>
2019-01-09 23:27:06 +0200

    Producer Consumer Multithreaded

---
+++ ProducerConsumer/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class Message
+    {
+        public DateTime TimeStamp { get; set; }
+        public int Sevirity { get; set; }
+        public string text { get; set; }
+    }
+}
