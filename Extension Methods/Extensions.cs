1a57b0e8dc73a48fe8fb148b6a342aa36641788c
Vasserman2000 <vasserman2000@gmail.com>
2019-02-13 07:02:04 +0200

    Extension Methods begining

--- Extension Methods/Extensions.cs
+++ Extension Methods/Extensions.cs
@@ -8,5 +8,8 @@ namespace Extension_Methods
 {
     public class Extensions
     {
+        // Extension methods allow us to add methods to an existing class
+        // without changing its source code or creating a new class that inherits from it
+
     }
 }
