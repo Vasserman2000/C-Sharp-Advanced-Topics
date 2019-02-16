edfeeea6439fed05bc9d127fa3f74909bd9da49a
Vasserman2000 <vasserman2000@gmail.com>
2019-02-16 22:10:18 +0200

    Events ...

--- C_Sharp_Advanced_Subjects/Program.cs
+++ C_Sharp_Advanced_Subjects/Program.cs
@@ -7,6 +7,7 @@ using Dynamic;
 using System.Linq;
 using Exception_Handling;
 using System.IO;
+using Events;
 
 namespace C_Sharp_Advanced_Subjects
 {
@@ -14,139 +15,176 @@ namespace C_Sharp_Advanced_Subjects
     {
         static void Main(string[] args)
         {
-            // LINQ Examples
-            // (without LINQ:)
-            var books = new BookRepository().GetBooks();
-            /*var cheapBooks = new List<Book>();
+            #region LINQ
 
-            foreach (Book book in books)
-            {
-                if (book.Price < 10)
-                    cheapBooks.Add(book);
-            }*/
-            // (with LINQ extension methods)
-            var cheapBooks = books
-                                .Where((b) => (b.Price < 10))
-                                .OrderBy(b => b.Title);
-            var title = books.Select(b => b.Title);
+            //// (without LINQ:)
+            //var books = new BookRepository().GetBooks();
+            ///*var cheapBooks = new List<Book>();
 
-            foreach (Book book in cheapBooks)
-            {
-                Console.WriteLine(book.Title + " " + book.Price);
-            }
-            // (with LINQ query operators)
-            var cheaperBooks = from b in books
-                               where b.Price < 10
-                               orderby b.Title
-                               select b.Title;
+            //foreach (Book book in books)
+            //{
+            //    if (book.Price < 10)
+            //        cheapBooks.Add(book);
+            //}*/
+            //// (with LINQ extension methods)
+            //var cheapBooks = books
+            //                    .Where((b) => (b.Price < 10))
+            //                    .OrderBy(b => b.Title);
+            //var title = books.Select(b => b.Title);
 
-            var specificBook  = books.Single(b => b.Title == "JavaScript: the weird parts"); //not safe (seq. cont. no match)
-            var specificBook2 = books.SingleOrDefault(b => b.Title == "JavaScript: the weird partss"); // safe
-            var specificBook3 = books.First(b => b.Title == "Hadoop for beginners"); //not safe
-            var specificBook4 = books.FirstOrDefault(b => b.Title == "Hadoop for beginners"); //safe
-            var specificBook5 = books.Last(b => b.Title == "Hadoop for beginners"); //not safe
-            var specificBook6 = books.LastOrDefault(b => b.Title == "Hadoop for beginners"); //safe
-            var specificBooks = books.Skip(2).Take(3);
-            var numberOfBooks = books.Count();
-            var highestPrice = books.Max(b => b.Price);
-            var lowestPrice = books.Min(b => b.Price);
-            var cheapestBook = books.Where(b => b.Price == lowestPrice);
-            var totalPrices = books.Sum(b => b.Price);
-            var averagePrice = books.Average(b => b.Price);
+            //foreach (Book book in cheapBooks)
+            //{
+            //    Console.WriteLine(book.Title + " " + book.Price);
+            //}
+            //// (with LINQ query operators)
+            //var cheaperBooks = from b in books
+            //                   where b.Price < 10
+            //                   orderby b.Title
+            //                   select b.Title;
 
-            // Lambdas
-            LambdaExpressions.LambdasTest.TestLambda();
+            //var specificBook  = books.Single(b => b.Title == "JavaScript: the weird parts"); //not safe (seq. cont. no match)
+            //var specificBook2 = books.SingleOrDefault(b => b.Title == "JavaScript: the weird partss"); // safe
+            //var specificBook3 = books.First(b => b.Title == "Hadoop for beginners"); //not safe
+            //var specificBook4 = books.FirstOrDefault(b => b.Title == "Hadoop for beginners"); //safe
+            //var specificBook5 = books.Last(b => b.Title == "Hadoop for beginners"); //not safe
+            //var specificBook6 = books.LastOrDefault(b => b.Title == "Hadoop for beginners"); //safe
+            //var specificBooks = books.Skip(2).Take(3);
+            //var numberOfBooks = books.Count();
+            //var highestPrice = books.Max(b => b.Price);
+            //var lowestPrice = books.Min(b => b.Price);
+            //var cheapestBook = books.Where(b => b.Price == lowestPrice);
+            //var totalPrices = books.Sum(b => b.Price);
+            //var averagePrice = books.Average(b => b.Price);
 
-            // Delegates:
-            Func<decimal, decimal, decimal> operationsHandler = Operations.Sum;
-            operationsHandler += Operations.Substruct;
+            #endregion
 
-            Calculation.Calculate(15.5m, 0.6m, operationsHandler);
+            #region Lambda Expressions
 
-            // Indexers:
-            Person person = new Person("Senior Developer", "Johnny", 48, "CodeOasis");
+            //LambdaExpressions.LambdasTest.TestLambda();
 
-            person[0] = "מר.";
+            #endregion
 
-            person[1] = "גברת";
+            #region Delegates
 
-            person[2] = "סיר";
+            //Func<decimal, decimal, decimal> operationsHandler = Operations.Sum;
+            //operationsHandler += Operations.Substruct;
 
-            // Generics:
-            GenericArray<int> intArray = new GenericArray<int>(Globals.ARRAY_SIZE);
-            Random rand = new Random();
-            for (int i = 0; i < Globals.ARRAY_SIZE; i++)
-            {
-                intArray.SetItem(rand.Next(1, 9), i);
-                Console.WriteLine(intArray.GetItem(i));
-            }
+            //Calculation.Calculate(15.5m, 0.6m, operationsHandler);
 
-            GenericArray<char> charArray = new GenericArray<char>(Globals.ARRAY_SIZE);
-            for (int i = 0; i < Globals.ARRAY_SIZE; i++)
-            {
-                int randomIndex = rand.Next(0, Globals.CHARS.Length);
-                charArray.SetItem(Globals.CHARS[randomIndex], i);
-                Console.WriteLine(charArray.GetItem(i));
-            }
+            #endregion
 
-            var number = new Generics.Nullable<int>();
-            Console.WriteLine("Has value? " + number.HasValue);
-            Console.WriteLine("Value: " + number.GetValueOrDefault());
+            #region Indexers
 
-            var isAnimal = new Generics.Nullable<bool>();
-            Console.WriteLine("Has value? " + isAnimal.HasValue);
-            Console.WriteLine("Value: " + isAnimal.GetValueOrDefault());
+            //Person person = new Person("Senior Developer", "Johnny", 48, "CodeOasis");
 
-            // Dynamic types:
-            DynamicType dType = new DynamicType();
-            dType.CheckType();
+            //person[0] = "מר.";
 
-            dType.dynamicField = 4;
-            int sum = dType.dynamicField + 17;
-            Console.WriteLine("The sum is: " + sum);
-            dynamic dynamicVariable = 1;
-            Console.WriteLine(dynamicVariable.GetType().ToString());
+            //person[1] = "גברת";
 
-            DynamicType.PrintValue("Hello World!!");
-            DynamicType.PrintValue(100);
-            DynamicType.PrintValue(100.50);
-            DynamicType.PrintValue(true);
-            DynamicType.PrintValue(DateTime.Now);
+            //person[2] = "סיר";
 
-            // Exceptions Handling:
-            try
-            {
-                ExceptionHandling.Calculator calcul = new ExceptionHandling.Calculator();
-                var result = calcul.Divide(4, 1);
-                //using (var streamReader = new StreamReader("C:/document.txt"))
-                //{
-                //    var content = streamReader.ReadToEnd();
-                //}
+            #endregion
 
-                var api = new YouTubeApi();
-                var videos = api.GetVideos(null);
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("You cannot divide by zero!");
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("File not found");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                //StreamReader.Dispose();
-            }
+            #region Generics
 
-            // Extension Methods:
-            string blogPost = "This is a really long blog post with a lots of sentences and paragraphs we would like to shorten";
+            //GenericArray<int> intArray = new GenericArray<int>(Globals.ARRAY_SIZE);
+            //Random rand = new Random();
+            //for (int i = 0; i < Globals.ARRAY_SIZE; i++)
+            //{
+            //    intArray.SetItem(rand.Next(1, 9), i);
+            //    Console.WriteLine(intArray.GetItem(i));
+            //}
 
-            string shortenedBolgPost = blogPost.Shorten(6);
+            //GenericArray<char> charArray = new GenericArray<char>(Globals.ARRAY_SIZE);
+            //for (int i = 0; i < Globals.ARRAY_SIZE; i++)
+            //{
+            //    int randomIndex = rand.Next(0, Globals.CHARS.Length);
+            //    charArray.SetItem(Globals.CHARS[randomIndex], i);
+            //    Console.WriteLine(charArray.GetItem(i));
+            //}
+
+            //var number = new Generics.Nullable<int>();
+            //Console.WriteLine("Has value? " + number.HasValue);
+            //Console.WriteLine("Value: " + number.GetValueOrDefault());
+
+            //var isAnimal = new Generics.Nullable<bool>();
+            //Console.WriteLine("Has value? " + isAnimal.HasValue);
+            //Console.WriteLine("Value: " + isAnimal.GetValueOrDefault());
+
+            #endregion
+
+            #region Dynamic Types
+
+            //DynamicType dType = new DynamicType();
+            //dType.CheckType();
+
+            //dType.dynamicField = 4;
+            //int sum = dType.dynamicField + 17;
+            //Console.WriteLine("The sum is: " + sum);
+            //dynamic dynamicVariable = 1;
+            //Console.WriteLine(dynamicVariable.GetType().ToString());
+
+            //DynamicType.PrintValue("Hello World!!");
+            //DynamicType.PrintValue(100);
+            //DynamicType.PrintValue(100.50);
+            //DynamicType.PrintValue(true);
+            //DynamicType.PrintValue(DateTime.Now);
+
+            #endregion
+
+            #region Exceptions Handling
+
+            //try
+            //{
+            //    ExceptionHandling.Calculator calcul = new ExceptionHandling.Calculator();
+            //    var result = calcul.Divide(4, 1);
+            //    //using (var streamReader = new StreamReader("C:/document.txt"))
+            //    //{
+            //    //    var content = streamReader.ReadToEnd();
+            //    //}
+
+            //    var api = new YouTubeApi();
+            //    var videos = api.GetVideos(null);
+            //}
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("You cannot divide by zero!");
+            //}
+            //catch (FileNotFoundException)
+            //{
+            //    Console.WriteLine("File not found");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    //StreamReader.Dispose();
+            //}
+
+            #endregion
+
+            #region Extension Methods
+
+            //string blogPost = "This is a really long blog post with a lots of sentences and paragraphs we would like to shorten";
+
+            //string shortenedBolgPost = blogPost.Shorten(6);
+
+            #endregion
+
+            #region Events
+
+            var videoEncoder = new VideoEncoder();
+            var mailService = new MailService();
+            var messageService = new MessageService();
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(new Events.Video() { Title = "A Nightmare on Elm Street" });
+
+            #endregion
 
             Console.ReadKey();
         }
